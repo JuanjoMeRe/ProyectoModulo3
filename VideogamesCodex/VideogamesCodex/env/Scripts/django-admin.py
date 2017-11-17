2a27918a1908bfa10311211ce1be371485839622
Juanjo <juanjodesarrollovideojuegos@outlook.es>
2017-11-17 17:53:48 +0100

    Proyecto Modulo 3 Django

---
+++ VideogamesCodex/VideogamesCodex/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\D.VIDEOJUEGOS\source\repos\VideogamesCodex\VideogamesCodex\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
